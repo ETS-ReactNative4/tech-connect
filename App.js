737bdedf51130039a6e48ca6c71a2604e15bce7b
Kylie Stanley <knstanley11@gmail.com>
2019-02-13 11:56:44 -0700

    Add navigation to App

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import Login from './Login'
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+
+const AppNavigator = createStackNavigator(
+  {Home: Login},
+  {headerMode: 'none'}
+)
+
+
+const AppContainer =  createAppContainer(AppNavigator)
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppContainer />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
