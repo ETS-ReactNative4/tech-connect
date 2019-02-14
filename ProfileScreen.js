4022e36d90a7b285645a4742c989c6a60beb0d0c
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-14 11:31:24 -0700

    Bring in react native elements

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
 
 export default class ProfileScreen extends React.Component {
   render() {
     return (
       <View style={styles.container} >
-        <TextInput style={styles.profileInput} placeholder="Name"/>
-        <TextInput style={styles.profileInput} placeholder="Phone Number"/>
-        <TextInput style={styles.profileInput} placeholder="Location"/>
-        <TextInput style={styles.profileInput} placeholder="Position"/>
-        <TextInput style={styles.profileInput} placeholder="Company"/>
-        <TextInput style={styles.profileInput} placeholder="GitHub"/>
-        <TextInput style={styles.profileInput} placeholder="LinkedIn"/>
-        <TextInput style={styles.profileInput} placeholder="Bio"/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Name" leftIcon={styles.leftIcon} leftIcon={{type: 'font-awesome', name: 'chevron-left'}}/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Phone Number"/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Location"/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Position"/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Company"/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="GitHub"/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="LinkedIn"/>
+        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Bio"/>
       </View>  
     )
   }
@@ -26,11 +28,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  profileInput: {
+  profileInputContainer: {
     width: 300,
     height: 50,
+    marginBottom: 10,
+  },
+  profileInput: {
     borderBottomWidth: 1.3,
     borderBottomColor: '#4AA9C5',
-    marginBottom: 10
+  },
+  leftIcon: {
+    color: 'blue'
   }
+
 })
