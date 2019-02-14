45f1b79a64efd9b9fcd2139bed66698d04a402f9
Kylie Stanley <knstanley11@gmail.com>
2019-02-14 16:55:17 -0700

    Add configuration for redux tools

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import LoginScreen from './LoginScreen'
 import ProfileScreen from './ProfileScreen'
+import HomeScreen from './HomeScreen'
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
@@ -15,10 +16,11 @@ const AppNavigator = createStackNavigator(
   {
     Login: LoginScreen,
     Profile: ProfileScreen,
+    Home: HomeScreen
   },
   {
     headerMode: 'none',
-    initialRouteName: "Profile"
+    initialRouteName: "Login"
   }
 )
 
