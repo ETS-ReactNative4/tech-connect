3b023f90544a227ce5596c8429dcd96da9647b8c
Kylie Stanley <knstanley11@gmail.com>
2019-02-13 17:30:10 -0700

    Install redux middleware

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import Login from './Login'
+import { LoginScreen } from './LoginScreen'
+import ProfileScreen from './ProfileScreen'
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const AppNavigator = createStackNavigator(
-  {Home: Login},
-  {headerMode: 'none'}
+  {
+    Login: LoginScreen,
+    Profile: ProfileScreen
+  },
+  {
+    headerMode: 'none',
+    initialRouteName: "Login"
+  }
 )
 
 
-const AppContainer =  createAppContainer(AppNavigator)
+const AppContainer = createAppContainer(AppNavigator)
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppContainer />
+      <Provider store={ store }>
+        <AppContainer />
+      </Provider>
     );
   }
 }
