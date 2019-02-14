578eca748b26bed4c0de25e9b708da873416ab9d
Kylie Stanley <knstanley11@gmail.com>
2019-02-14 16:51:49 -0700

    Add functionality to switch to home from profile screen

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { Input } from 'react-native-elements';
+import { updateUser } from './thunks/updateUser'
+import { connect } from 'react-redux'
 
-export default class ProfileScreen extends React.Component {
+
+export class ProfileScreen extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      bio: '',
+      linkedin: '',
+      github: '',
+      phone_number: '',
+      location: 'Denver,CO',
+      employer: 'Turing',
+      position: 'Employee'
+    }
+  }
+
+  handleSave = () => {
+    this.props.updateUser({...this.state, api_key: this.props.user.api_key})
+    this.props.navigation.navigate('Home')
+  }
+
   render() {
     return (
+
       <View style={styles.container} >
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Name" 
+        <Input
+          onChangeText={(text) => this.setState({name: text})} 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="Name" 
           leftIcon={
             <Icon
               name='user'
@@ -16,8 +44,12 @@ export default class ProfileScreen extends React.Component {
             />
           }
         />
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Phone Number"
-        leftIcon={
+        <Input 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="Phone Number"
+          leftIcon={
             <Icon
               name='phone'
               size={18}
@@ -25,7 +57,11 @@ export default class ProfileScreen extends React.Component {
             />
           }
           />
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Location"
+        <Input 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="Location"
           leftIcon={
             <Icon
               name='map-pin'
@@ -34,7 +70,11 @@ export default class ProfileScreen extends React.Component {
             />
           }
         />
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Position"
+        <Input 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="Position"
           leftIcon={
             <Icon
               name='briefcase'
@@ -43,7 +83,11 @@ export default class ProfileScreen extends React.Component {
             />
           }
         />
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Company"
+        <Input 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="Company"
           leftIcon={
             <Icon
               name='home'
@@ -52,7 +96,11 @@ export default class ProfileScreen extends React.Component {
             />
           }
         />
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="GitHub"
+        <Input 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="GitHub"
           leftIcon={
             <Icon
               name='github'
@@ -61,7 +109,11 @@ export default class ProfileScreen extends React.Component {
             />
           }
         />
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="LinkedIn"
+        <Input 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="LinkedIn"
           leftIcon={
             <Icon
               name='linkedin'
@@ -70,7 +122,11 @@ export default class ProfileScreen extends React.Component {
             />
           }
         />
-        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Bio"
+        <Input 
+          containerStyle={styles.profileInputContainer} 
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.profileInput} 
+          placeholder="Bio"
           leftIcon={
             <Icon
               name='edit'
@@ -79,10 +135,25 @@ export default class ProfileScreen extends React.Component {
             />
           }
         />
+        <TouchableHighlight style={styles.button}>
+          <Button title="save" color='#E8FDFF' onPress={this.handleSave}/>
+        </TouchableHighlight>
       </View>  
     )
   }
 }
+
+export const mapStateToProps = (state) => ({
+  user: state.user,
+  isLoading: state.isLoading,
+  error: state.error
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  updateUser: (user) => dispatch(updateUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileScreen)
 
 
 
@@ -104,6 +175,11 @@ const styles = StyleSheet.create({
   icon: {
     marginRight: 10,
     marginLeft: 0
+  },
+  button: {
+    backgroundColor: '#8FC9DB',
+    borderRadius: 25,
+    height: 40,
+    width: 140
   }
-
 })
