05210ba8b0593cc928412e279c57283dcd7763e0
Kylie Stanley <knstanley11@gmail.com>
2019-02-14 16:48:12 -0700

    Add thunk for update user

---
+++ thunks/updateUser.js
@@ -0,0 +1,33 @@
+import { isLoading, hasErrored, updateUserAction } from '../actions'
+
+export const updateUser = (user) => {
+  console.log(user)
+  const url = 'https://tech-connect-be.herokuapp.com/api/v1/users'
+  return async (dispatch) => {
+
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+
+      if(!response.ok) {
+        console.log(response)
+        const errorText = await response.json()
+        throw Error(errorText.error)
+      }
+
+      dispatch(isLoading(false))
+      const updatedUser = await response.json()
+      dispatch(updateUserAction(updatedUser.data.attributes))
+      dispatch(hasErrored(''))
+    } catch(err) {
+      console.log(err)
+      dispatch(hasErrored(err.message))
+    }
+  }
+}
