c2b327b2003b5fba764d0b33e5181cf14a428200
Kylie Stanley <knstanley11@gmail.com>
2019-02-13 17:29:03 -0700

    Add actions

---
+++ actions/index.js
@@ -0,0 +1,23 @@
+export const loginUser = (user) => ({
+  type: 'LOG_IN',
+  user
+})
+
+export const logoutUser = () => ({
+  type: 'LOG_OUT'
+})
+
+export const updateUser = (user) => ({
+  type: 'UDPATE_USER',
+  user
+})
+
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  isLoading: bool
+})
+
+export const hasErrored = (message) => ({
+  type: 'HAS_ERRORED',
+  message
+})
