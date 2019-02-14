db614dce685a364e415cbe0a4d2788e8dcf96021
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-14 12:08:02 -0700

    Add icons for inputs

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,20 +1,84 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Feather';
 import { Input } from 'react-native-elements';
 
 export default class ProfileScreen extends React.Component {
   render() {
     return (
       <View style={styles.container} >
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Name" leftIcon={styles.leftIcon} leftIcon={{type: 'font-awesome', name: 'chevron-left'}}/>
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Phone Number"/>
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Location"/>
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Position"/>
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Company"/>
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="GitHub"/>
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="LinkedIn"/>
-        <Input containerStyle={styles.profileInputContainer} inputContainerStyle={styles.profileInput} placeholder="Bio"/>
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Name" 
+          leftIcon={
+            <Icon
+              name='user'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+        />
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Phone Number"
+        leftIcon={
+            <Icon
+              name='phone'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+          />
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Location"
+          leftIcon={
+            <Icon
+              name='map-pin'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+        />
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Position"
+          leftIcon={
+            <Icon
+              name='briefcase'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+        />
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Company"
+          leftIcon={
+            <Icon
+              name='home'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+        />
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="GitHub"
+          leftIcon={
+            <Icon
+              name='github'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+        />
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="LinkedIn"
+          leftIcon={
+            <Icon
+              name='linkedin'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+        />
+        <Input containerStyle={styles.profileInputContainer} leftIconContainerStyle={styles.icon} inputContainerStyle={styles.profileInput} placeholder="Bio"
+          leftIcon={
+            <Icon
+              name='edit'
+              size={18}
+              color='#4AA9C5'
+            />
+          }
+        />
       </View>  
     )
   }
@@ -37,8 +101,9 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1.3,
     borderBottomColor: '#4AA9C5',
   },
-  leftIcon: {
-    color: 'blue'
+  icon: {
+    marginRight: 10,
+    marginLeft: 0
   }
 
 })
