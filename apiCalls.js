d651f948c2f440b4d5e559186925c6c9e9aa2d7c
Kylie Stanley <knstanley11@gmail.com>
2019-02-16 13:56:15 -0700

    Add function to get all employers

--- apiCalls.js
+++ apiCalls.js
@@ -7,3 +7,14 @@ getLocations = async () => {
     })
     this.setState({ locations })
   }
+
+
+getEmployers = async () => {
+  const url = 'https://tech-connect-be.herokuapp.com/api/v1/employers'
+  const response = await fetch(url)
+  const locationData = await response.json()
+  const locations = locationData.data.map(location => {
+    return {label: location.attributes.name}
+  })
+  this.setState({ locations })
+}
