4cbc9e166c8a2391fa051d15e5b88a25dfb50e00
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-16 10:08:56 -0700

    Add styling to suggested connections component

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableHighlight, Image } from 'react-native';
 import { connect } from 'react-redux';
-import { SuggestedConnection } from './SuggestedConnection.js' 
+import SuggestedConnection from './SuggestedConnection.js' 
 
 export class HomeScreen extends React.Component {
   constructor(props) {
@@ -14,13 +14,13 @@ export class HomeScreen extends React.Component {
       <View style={styles.container}>
         <Text style={styles.name}>Joe Jonas</Text>
         <Text style={styles.position}>Singer</Text>
-        <View>
+        <View style={styles.imageContainer}>
           <Image source={require('./profile-pic.jpeg')} style={styles.profilePic} />
-          <View style={styles.circle}/>
         </View>
+        <View style={styles.circle}/>
         <View styles={styles.suggestedConnections}>
-          <Text>Suggested Connections</Text>
-          {/* Render suggested connections here */}
+          <Text style={styles.suggestedTitle}>Suggested Connections</Text>
+          <SuggestedConnection />
         </View>
       </View>
     )
@@ -36,34 +36,44 @@ export default connect(mapStateToProps)(HomeScreen)
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     alignItems: 'flex-start',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+    padding: 20,
+    height: '100%',
+    width: '100%'
   },
   circle: {
     position: 'absolute',
-    top: -480,
+    top: -380,
     left: -93,
     height: 600,
     width: 600,
     borderRadius: 300,
     backgroundColor: '#4AA9C5',
-    shadowOffset: {  width: 0,  height: 2,  },
+    shadowOffset: {  width: 0,  height: 2  },
     shadowRadius: 10,
     shadowColor: 'black',
     shadowOpacity: .5,
+  },
+  imageContainer: {
+    display: 'flex',
+    height: 180,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 200000,
+    marginBottom: 20,
   },
   profilePic: {
     height: 180,
     width: 180,
     borderRadius: 90,
-    position: 'absolute',
     zIndex: 20000,
-    top: 20,
-    left: 117
   },
   name: {
-    marginTop: 50,
-    marginLeft: 25,
+    marginTop: 20,
     position: 'relative',
     zIndex: 20000,
     color: 'white',
@@ -71,11 +81,18 @@ const styles = StyleSheet.create({
   },
   position: {
     marginTop: 5,
-    marginLeft: 25,
     position: 'relative',
     zIndex: 20000,
     color: 'white',
     fontSize: 20,
-
+  },
+  suggestedTitle: {
+    color: '#4AA9C5',
+    fontSize: 20,
+    marginBottom: 10
+  },
+  suggestedConnections: {
+    display: 'flex',
+    alignItems: 'stretch'
   }
 })
--- SuggestedConnection.js
+++ SuggestedConnection.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
 
 export default class SuggestedConnection extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Image source={require('./profile-pic.jpeg')} style={styles.picture} />
-        <Text>Nick Jonas</Text>
-        <Text>Singer</Text>
-        <Button></Button>
+        <Text style={styles.name}>Nick Jonas</Text>
+        <Text style={styles.position}>Singer</Text>
+        <Button title='Connect' style={styles.connectBtn}/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#4AA9C5',
+    height: 126,
+    shadowOffset: {  width: 0,  height: 2 },
+    shadowRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: .5,
+    marginLeft: 50,
+    marginRight: 50
+  },
+  picture: {
+    height: 90,
+    width: 90,
+    borderRadius: 50,
+    position: 'absolute',
+    left: -40,
+    top: 18
+  },
+  name: {
+
+  },
+  position: {
+
+  },
+  connectBtn: {
+    position: 'absolute',
+    right: -110
+  }
+})
