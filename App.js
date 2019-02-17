ad92602179530646fb500229c5bd4f322317d40d
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-16 20:57:58 -0700

    Add pages to navigate to

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import LoginScreen from './LoginScreen'
 import ProfileScreen from './ProfileScreen'
+import ProfilePage from './ProfilePage'
 import HomeScreen from './HomeScreen'
+import MessagesScreen from './MessagesScreen'
+import ScheduleScreen from './ScheduleScreen'
 import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
-import { Navigator } from 'react-native'
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const AppNavBar = createBottomTabNavigator(
   {
     Home: HomeScreen,
-    Profile: ProfileScreen
+    Profile: ProfilePage,
+    Messages: MessagesScreen,
+    Schedule: ScheduleScreen
   }
 )
 
 const AppNavigator = createStackNavigator(
   {
     Login: LoginScreen,
-    Home: AppNavBar
+    Home: AppNavBar,
   },
   {
     headerMode: 'none',
---
+++ ScheduleScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class ScheduleScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the schedule page</Text>
+      </View>
+    )
+  }
+}
