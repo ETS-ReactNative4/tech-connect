7c334f50d9962dd99e144377909b8f1eab6d3312
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-17 09:14:33 -0700

    Add icons to nav bar

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import ProfilePage from './ProfilePage'
 import HomeScreen from './HomeScreen'
 import MessagesScreen from './MessagesScreen'
 import ScheduleScreen from './ScheduleScreen'
+import Icon from 'react-native-vector-icons/Feather';
 import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux';
@@ -16,17 +17,46 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 
 const AppNavBar = createBottomTabNavigator(
   {
-    Home: HomeScreen,
+    Home: {
+      screen: HomeScreen
+    },
     Profile: ProfilePage,
     Messages: MessagesScreen,
     Schedule: ScheduleScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = 'home'
+        } else if (routeName === 'Profile') {
+          iconName = 'user'
+        } else if (routeName === 'Messages') {
+          iconName = 'message-circle'
+        } else if (routeName === 'Schedule') {
+          iconName = 'calendar'
+        }
+
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#4AA9C5',
+      inactiveTintColor: 'gray',
+    }
   }
 )
 
 const AppNavigator = createStackNavigator(
   {
     Login: LoginScreen,
-    Home: AppNavBar,
+    NavBar: AppNavBar,
+    Profile: ProfileScreen,
+    ProfilePage: ProfilePage,
+    Messages: MessagesScreen,
+    Schedule: ScheduleScreen
   },
   {
     headerMode: 'none',
