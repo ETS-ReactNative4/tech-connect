9407f2aa7263adbb82614e37ee9e8b191bbbadf7
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-17 13:21:36 -0700

    Add Connections component

---
+++ Connection.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+
+export default class Connection extends Component {
+  render() {
+    return (
+      <View style={ styles.container }>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+})
