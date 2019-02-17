0cbcd80659731509b1be4fd5d9228ce9e8825c1a
Kylie Stanley <knstanley11@gmail.com>
2019-02-17 15:55:48 -0700

    Add new icons and gradients to login page

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
 import { connect } from 'react-redux'
+import { LinearGradient } from 'expo'
+import { Input } from 'react-native-elements';
 import { createUser } from './thunks/createUser'
 import { loginUser } from './thunks/loginUser'
 
@@ -12,13 +15,15 @@ export class LoginScreen extends React.Component {
       register: false,
       email: '',
       password: '',
-      password_confirmation: ''
+      password_confirmation: '',
+      error: false
     }
   }
 
   toggleRegister = () => {
     this.setState({
-      register: !this.state.register
+      register: !this.state.register,
+      error: false
     })
   }
 
@@ -34,6 +39,10 @@ export class LoginScreen extends React.Component {
       await this.props.loginUser(loginInfo)
       this.handleUpdateProfile()
     }
+
+    if(this.props.error) {
+      this.setState({ error: true })
+    }
   }
 
   handleUpdateProfile = () => {
@@ -47,22 +56,84 @@ export class LoginScreen extends React.Component {
   render() {
     const btnText = this.state.register ? 'REGISTER' : 'SIGN IN'
     const toggleText = this.state.register ? 'Sign In' : 'Register'
-
+    const errorMessage = this.state.error ? this.props.error : null
 
     return (
       <View style={styles.container}>
         <Text>Tech Connect</Text>
-        <TextInput placeholder="Email" style={styles.input} name='email' onChangeText={(text) => this.setState({email: text})} />
-        <TextInput placeholder="Password" secureTextEntry={true} style={styles.input} name='password' onChangeText={(text) => this.setState({password: text})} /> 
+        <Input
+          leftIcon= {
+            <LinearGradient
+                colors={['#4AA9C5', '#6364c1', '#93548F']}
+                start={0.4}
+                style={styles.gradient} >
+              <Icon
+                raised={true}
+                name='envelope'
+                size={28}
+                color='#FFF'
+              />
+            </LinearGradient>
+            }
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.inputContainer} 
+          containerStyle={styles.input} 
+          placeholder="Email" style={styles.input} 
+          name='email' onChangeText={(text) => this.setState({email: text})} 
+        />
+        <Input 
+          leftIcon= {
+            <LinearGradient
+              colors={['#4AA9C5', '#6364c1', '#93548F']}
+              start={0.4}
+              style={styles.gradient} >
+            <Icon
+              raised={true}
+              name='lock'
+              size={32}
+              color='#FFF'
+            />
+            </LinearGradient>
+          }
+          leftIconContainerStyle={styles.icon} 
+          inputContainerStyle={styles.inputContainer} 
+          containerStyle={styles.input} 
+          secureTextEntry={true} 
+          name='password' 
+          placeholder="Password" 
+          onChangeText={(text) => this.setState({password: text})} 
+        /> 
         {
-          this.state.register && <TextInput placeholder="Confirm Password" secureTextEntry={true} style={styles.input} onChangeText={(text) => this.setState({password_confirmation: text})}/> 
+          this.state.register && 
+            <Input 
+              leftIcon= {
+                <LinearGradient
+                  colors={['#4AA9C5', '#6364c1', '#93548F']}
+                  start={0.4}
+                  style={styles.gradient} >
+                  <Icon
+                    raised={true}
+                    name='lock'
+                    size={32}
+                    color='#FFF'
+                  />
+                </LinearGradient>
+              }
+              leftIconContainerStyle={styles.icon} 
+              inputContainerStyle={styles.inputContainer} 
+              containerStyle={styles.input} 
+              secureTextEntry={true} 
+              placeholder="Confirm Password" 
+              onChangeText={(text) => this.setState({password_confirmation: text})}
+            /> 
         }
+        <Text style={styles.error}>{ errorMessage }</Text>
         <TouchableHighlight style={styles.button}>
           <Button title={btnText} color='#E8FDFF' onPress={this.handleSubmit}/>
         </TouchableHighlight>
         <View style={styles.textContainer}>
           <TouchableOpacity onPress={this.toggleRegister} >
-            <Text style={[styles.text, {textDecorationLine: 'underline'}]}>{toggleText}</Text>
+            <Text style={[styles.text, {textDecorationLine: 'underline', fontWeight: 'bold'}]}>{toggleText}</Text>
           </TouchableOpacity>
           <Text style={styles.text} > if you do not have an account</Text>
         </View>
@@ -87,31 +158,61 @@ export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen)
 
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#4AA9C5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  gradient: {
+    borderRadius: 300, 
+    width: 48, 
+    height: 48, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
   text: {
     color: '#E8FDFF',
   },
+  icon: {
+    marginRight: 10,
+    marginLeft: -5,
+    width: 48,
+    height: 48,
+    borderRadius: 50,
+    backgroundColor: 'pink'
+  },
+  error: {
+    color: 'red',
+    marginBottom: 3
+  },
+  inputContainer: {
+    borderBottomWidth: 0,
+    height: 40
+  },
   input: {
     backgroundColor: '#E8FDFF',
-    borderRadius: 20,
+    borderRadius: 30,
     padding: 10,
-    height: 45,
     width: 300,
     margin: 10
   },
   button: {
+    justifyContent: 'center', 
+    alignItems: 'center',
     backgroundColor: '#8FC9DB',
     borderRadius: 25,
-    height: 40,
-    width: 140
+    height: 44,
+    width: 140,
+    shadowOffset: {  width: 2,  height: 2},
+    shadowRadius: 5,
+    shadowColor: 'black',
+    shadowOpacity: .2
   },
   textContainer: {
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    marginTop: 8
   }
 });
