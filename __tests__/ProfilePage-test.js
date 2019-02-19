16887989129971820ac7d61d9431ee6035c163c5
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-18 23:03:09 -0700

    Add test for viewProfile function

--- __tests__/ProfilePage-test.js
+++ __tests__/ProfilePage-test.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import ProfilePage from '../ProfilePage'
+import { ProfilePage, mapStateToProps } from '../ProfilePage'
 import { TouchableHighlight } from 'react-native'
 import Connection from '../Connection'
+import { getUserInfo } from '../apiCalls'
+
+jest.mock('../apiCalls')
 
 describe('ProfilePage', () => {
   let wrapper
@@ -23,7 +26,8 @@ describe('ProfilePage', () => {
       connections: []
     }
     mockNavigation = {
-      getParam: () => mockUser
+      navigate: jest.fn(),
+      getParam: () => mockUser,
     }
     wrapper = shallow(<ProfilePage user={ mockUser } navigation={ mockNavigation }/>)
   })
@@ -42,4 +46,55 @@ describe('ProfilePage', () => {
 
     expect(wrapper.find(Connection).length).toEqual(0)
   })
+
+  it('should render 1 connection', () => {
+    const mockUser = {
+      name: 'Howard',
+      position: {
+        job_title: 'Developer'
+      },
+      employer: {
+        name: 'Turing'
+      },
+      location: {
+        city: 'Denver'
+      },
+      connections: [{name: 'Howard', id: 3}]
+    }
+    const mockNavigation = {
+      getParam: () => mockUser
+    }
+    const wrapper = shallow(<ProfilePage user={ mockUser } navigation={ mockNavigation } />)
+
+    expect(wrapper.find(Connection).length).toEqual(1)
+  })
+
+  describe('viewProfile', () => {
+    it('should fire getUserInfo and navigation', async () => {
+      const wrapper = shallow(<ProfilePage user={ mockUser } navigation={{navigate: jest.fn(), getParam: jest.fn()}} />)
+      
+      await wrapper.instance().viewProfile(3)
+
+      expect(getUserInfo).toHaveBeenCalled()
+      expect(wrapper.instance().props.navigation.navigate).toHaveBeenCalled()
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return an object with a user key', () => {
+      const mockState = {
+        user: {},
+        isLoading: false,
+        hasErrored: false
+      }
+      const expected = {
+        user: {}
+      }
+
+      const result = mapStateToProps(mockState)
+
+      expect(result).toEqual(expected)
+    })
+  })
+
 })
