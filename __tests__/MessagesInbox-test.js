ec73e622d5a7ff05e63b12d77c006576e4987a2e
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-19 22:52:51 -0700

    Add test to check for the number of messages rendered

--- __tests__/MessagesInbox-test.js
+++ __tests__/MessagesInbox-test.js
@@ -14,4 +14,18 @@ describe('MessagesInbox', () => {
 
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('should render 0 Messages', () => {
+
+    expect(wrapper.find(Message).length).toEqual(0)
+  })
+
+  it('should render 1 Message component', async () => {
+    const mockMessages = [{message: 'This is a message'}]
+    await wrapper.setState({
+      messages: mockMessages
+    })
+
+    expect(wrapper.find(Message).length).toEqual(1)
+  })
 })
