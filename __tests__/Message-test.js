59fd8bb76c8ca19707f1699db33a207d2b178172
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-20 11:46:01 -0700

    Add test for set state

--- __tests__/Message-test.js
+++ __tests__/Message-test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Message from '../Message'
+import { TouchableOpacity, Text } from 'react-native'
 
 describe('Message', () => {
   let wrapper
@@ -18,5 +19,18 @@ describe('Message', () => {
   it('should match the snapshot', () => {
 
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should set state on press of the message and conditionally render a Text element', async () => {
+    
+    await wrapper.find(TouchableOpacity).simulate('press')
+
+    expect(wrapper.state().showMessage).toEqual(true)
+    expect(wrapper.find(Text).length).toEqual(4)
+
+    await wrapper.find(TouchableOpacity).simulate('press')
+
+    expect(wrapper.state().showMessage).toEqual(false)
+    expect(wrapper.find(Text).length).toEqual(3)
   })
 })
